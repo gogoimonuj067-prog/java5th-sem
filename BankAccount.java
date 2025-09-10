3381ecca97c2da0607dc7851351bf21f336fd00f
monuj gogoi <gogoimonuj067@gmail.com>
2025-09-11 00:47:23 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+    String name;
+    double balance;
+
+    BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    void deposit(double amt) { balance += amt; }
+
+    void withdraw(double amt) {
+        if (amt <= balance) balance -= amt;
+        else System.out.println("Insufficient Balance");
+    }
+
+    void show() {
+        System.out.println(name + " Balance: " + balance);
+    }
+}
+
+class BankTest {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount("John", 1000);
+        acc.deposit(500);
+        acc.withdraw(300);
+        acc.withdraw(1500);
+        acc.show();
+    }
+}
