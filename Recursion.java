bab06a4a1826bb3bdd144c7a86f561207c8ebd1d
monuj gogoi <gogoimonuj067@gmail.com>
2025-09-11 00:32:36 +0530

    Create Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("GCD of 24 and 36: " + gcd(24, 36));  
+    }
+}
