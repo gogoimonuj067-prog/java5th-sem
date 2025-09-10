e67a7a7a296de946b34da76f931dd92d0797df32
monuj gogoi <gogoimonuj067@gmail.com>
2025-09-11 00:11:14 +0530

    Create MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,16 @@
+public class MissingNumber {
+
+    public static int findMissingNumber(int[] arr, int n) {
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : arr) actualSum += num;
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6};
+        int missing = findMissingNumber(arr, 6);
+        System.out.println("Missing Number: " + missing); // Output: 3
+    }
+}
+
